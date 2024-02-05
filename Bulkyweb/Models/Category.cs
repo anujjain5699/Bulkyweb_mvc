2371a24f3a670b00f2beeb75524b78f642f5330a
anujjain5699 <anujjain5699@gmail.com>
2024-02-05 23:35:45 +0530

    Crud Operation

---
+++ Bulkyweb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulkyweb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order value range from 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
